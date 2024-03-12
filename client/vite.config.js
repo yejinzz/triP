e3dde2a3be6c0ad798d630d20b788199a7e69afc
yejinzz <yejin1320@naver.com>
2024-03-12 21:21:06 +0900

    chore: 환경변수 설정 및 VitePluginHtmlEnv 플러그인 적용

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import VitePluginHtmlEnv from "vite-plugin-html-env";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePluginHtmlEnv(),
+    VitePluginHtmlEnv({ compiler: true }),
+  ],
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
   },
