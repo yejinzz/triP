96fff61a1898b9e77e13b7506ef3064965219970
yejinzz <yejin1320@naver.com>
2024-03-26 23:18:13 +0900

    feat: useCloseModal 훅 생성

--- client/src/components/common/modal/Modal.jsx
+++ client/src/components/common/modal/Modal.jsx
@@ -1,30 +1,38 @@
 import styled from "styled-components";
+import useCloseModal from "@/hooks/useCloseModal";
 
-const Modal = ({ children }) => {
+const Modal = ({ children, modalRef = null }) => {
+  useCloseModal(modalRef);
   return (
-    <Background>
-      <ModalBox>
-        {children}
-        {/* <CloseButton onClick={() => dispatch(closeModal())}>닫기</CloseButton> */}
-      </ModalBox>
-    </Background>
+    <>
+      <Background></Background>
+      <ModalBox ref={modalRef}>{children}</ModalBox>
+    </>
   );
 };
 
 export default Modal;
 
 const Background = styled.div`
-  position: fixed;
+  /* position: fixed;
   top: 0;
   left: 0;
-  width: 100%;
-  height: 100%;
-  backdrop-filter: blur(10px);
+  width: 100vw;
+  height: 100vh;
+  backdrop-filter: blur(4px);
   -webkit-backdrop-filter: blur(4px);
-  background: rgba(0, 0, 0, 0.5);
+  background-color: rgba(0, 0, 0, 0.5);
   display: flex;
   align-items: center;
   justify-content: center;
+  z-index: 999; */
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  backdrop-filter: blur(4px);
+  background-color: #000000ac;
   z-index: 999;
 `;
 
@@ -32,10 +40,23 @@ const ModalBox = styled.div`
   /* position: relative; */
   /* box-shadow: -5px -5px 10px rgba(255, 255, 255, 0.8),
     5px 5px 10px rgba(0, 0, 0, 0.1); */
-  padding: 3rem;
+
+  /* padding: 3rem;
   border-radius: 20px;
   background: var(--color-bg-100);
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); */
+  z-index: 999;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding: 3rem;
+  /* width: 80vw;*/
+  /* width: 100vw; */
+  /* transform: translate(50%, 50%); */
+  background-color: #f3f3f3;
+  border-radius: 15px;
+  box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
 `;
 
 // const CloseButton = styled(MdClose)`
---
+++ client/src/hooks/useCloseModal.jsx
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+import { setCloseModal } from "@/store/slice/modalSlice";
+import { useDispatch } from "react-redux";
+
+const useCloseModal = (ref, handler) => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const listener = (e) => {
+      if (!ref.current || ref.current.contains(e.target)) {
+        return;
+      }
+
+      handler ? handler() : dispatch(setCloseModal());
+    };
+    document.addEventListener("mousedown", listener);
+    document.addEventListener("touchstart", listener);
+    return () => {
+      document.removeEventListener("mousedown", listener);
+      document.removeEventListener("touchstart", listener);
+    };
+  }, [ref, handler]);
+};
+
+export default useCloseModal;
