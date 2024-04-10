49b91022d57c7b0cdad10b335489ab7213d6e24c
yejinzz <yejin1320@naver.com>
2024-04-11 01:03:44 +0900

    feat: place model schema

---
+++ server/src/models/placeModel.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+//생성될 필드명을 정한다.
+const placeSchema = new Schema({
+  regionCode: String,
+  placeType: String,
+  placeInfo: {
+    name: String,
+    phoneNumber: String,
+    usageTime: String,
+    address: String,
+    imgUrl: String,
+    siteUrl: String,
+    bestMenu: String,
+    coords: { lat: Number, lng: Number },
+  },
+});
+
+module.exports = mongoose.model("Place", placeSchema);
