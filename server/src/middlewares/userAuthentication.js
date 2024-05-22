7451bc5a1d45c8f6126a825604533860307342f3
yejinzz <yejin1320@naver.com>
2024-05-22 23:20:57 +0900

    refactor: 유저 인증 에러 핸들링

--- server/src/middlewares/userAuthentication.js
+++ server/src/middlewares/userAuthentication.js
@@ -1,12 +1,18 @@
 const Account = require("../models/accountModel");
 
 exports.userAuthentication = async (req, res, next) => {
-  const refreshToken = req.cookies.refresh_token;
-  const findUser = await Account.findOne({ refreshToken }).exec();
-  if (!findUser) {
-    return res.status(404).json({ error: "사용자를 찾을 수 없습니다." });
+  try {
+    const refreshToken = req.cookies.refresh_token;
+    const findUser = await Account.findOne({ refreshToken }).exec();
+    if (!findUser) {
+      return res.status(404).json({ error: "사용자를 찾을 수 없습니다." });
+    }
+
+    req.user = findUser;
+    next();
+  } catch (err) {
+    // 에러 발생 시 500 에러 반환
+    console.error(err);
+    res.status(500).json({ error: "서버 에러" });
   }
-  req.token = refreshToken;
-  req.user = findUser;
-  next();
 };
